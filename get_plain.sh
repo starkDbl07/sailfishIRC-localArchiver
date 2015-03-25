16063e006b0576a995bc7232b92394fecf32e4ef
starkDbl07 <abhi.emailto@gmail.com>
2015-03-25 17:01:46 +0545

    get all newer archives

--- get_plain.sh
+++ get_plain.sh
@@ -1,6 +1,41 @@
 #!/bin/bash
 
 archive_dir="archive"
-#cat \#sailfishos-porters.2014-06-26.log.html | grep 'class="nick"' | sed -e 's^<tr id="t\([^"]*\)"><[^>]*>\([^<]*\)</th><td[^>]*>\(.*\)^\1  \2    \3^; s^</td><[^<]*><[^<]*>[^>]*</a></td></tr>$^^'
+temp_dir="temp"
 
-curl 'http://www.merproject.org/logs/%23sailfishos-porters/%23sailfishos-porters.2014-06-26.log.html' | grep 'class="nick"' | sed -e 's^<tr id="t\([^"]*\)"><[^>]*>\([^<]*\)</th><td[^>]*>\(.*\)^\1  \2    \3^; s^</td><[^<]*><[^<]*>[^>]*</a></td></tr>$^^' > $archive_dir/2014-06-26.txt
+mkdir -p "$temp_dir"
+
+function getArchiveForDate {
+	date="$1"
+	curl -s "http://www.merproject.org/logs/%23sailfishos-porters/%23sailfishos-porters.$date.log.html" | grep 'class="nick"' | sed -e 's^<tr id="t\([^"]*\)"><[^>]*>\([^<]*\)</th><td[^>]*>\(.*\)^\1  \2    \3^; s^</td><[^<]*><[^<]*>[^>]*</a></td></tr>$^^' > $archive_dir/$date.txt
+}
+
+function getIndexes {
+	curl -s "http://www.merproject.org/logs/%23sailfishos-porters/index.html" | grep '</li>' | sed -n '2,$p'| awk -F'>' '{print $3}' | awk '{print $1}' | sort -n
+}
+
+function main {
+	echo "Getting Date Indexes..."
+	getIndexes > $temp_dir/indexes
+
+	echo "Fetching Archive as text..."
+	count=0
+	while read index
+	do 
+		if [ ! -e "$archive_dir/$index.txt" ]
+		then
+			let count=count+1
+			echo -e "\t - $index"
+			getArchiveForDate $index
+		fi
+	done < $temp_dir/indexes
+
+	if [ $count -eq 0 ]
+	then
+		echo ""
+		echo "No newer archive found."
+		echo ""
+	fi
+}
+
+main
