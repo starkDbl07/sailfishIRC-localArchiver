253409b4b2eeec95b40be57bad2e61eed948b81b
starkDbl07 <abhi.emailto@gmail.com>
2015-03-26 01:02:34 +0545

    added pasties piratepad and opensuse

--- getArchive.sh
+++ getArchive.sh
@@ -12,6 +12,9 @@ mkdir -p "$temp_dir"
 mkdir -p "$archive_dir"
 mkdir -p "$pastebin_dir"
 
+source includes/timestamps.func
+source includes/pasties.func
+
 function getArchiveForDate {
 	date="$1"
 	#curl -s "http://www.merproject.org/logs/%23sailfishos-porters/%23sailfishos-porters.$date.log.html" | grep 'class="nick"' | sed -e 's^<tr id="t\([^"]*\)"><[^>]*>\([^<]*\)</th><td[^>]*>\(.*\)^\1  \2    \3^; s^</td><[^<]*><[^<]*>[^>]*</a></td></tr>$^^' > $archive_dir/$date.txt
@@ -20,45 +23,6 @@ function getArchiveForDate {
 
 function getIndexes {
 	curl -s "http://www.merproject.org/logs/%23sailfishos-porters/index.html" | grep '</li>' | sed -n '2,$p'| awk -F'>' '{print $3}' | awk '{print $1}' | sort -n
-}
-
-
-function genPastebinLinks {
-	grep '<a href="http://pastebin' "$1" | sed 's~^\([^\ *]*\)\ .*<a href="\(http://pastebin.[^"]*\).*~\1 \2~g' | awk -F'/' '{print $1"//"$3"/raw.php?i="$4" "$4}'
-}
-
-function downloadPastebinLink {
-	params=( `echo $1` )
-	file="$pastebin_dir/${params[0]}_${params[2]}"
-	link="${params[1]}"
-	#echo "$file:$link"
-	if [ ! -e "$file" ]
-	then
-		curl -s -o "$file" "$link"
-		if [ -e "$file" ]
-		then
-			echo -e "- ${params[2]}"
-		fi
-	fi
-}
-
-function genAllPastebinsLinks {
-	for archive in `ls $archive_dir/`
-	do
-		genPastebinLinks "$archive_dir/$archive"
-	done 
-}
-
-function getAllPastebins {
-	: > $temp_dir/pastebins
-	echo "Extracting Pastebin Links from chat archive..."
-	genAllPastebinsLinks | sort | uniq | grep -v '^$' > $temp_dir/pastebins
-	echo "Fetching pastebins..."
-	while read link
-	do
-		#echo "$link"
-		downloadPastebinLink "$link"
-	done < $temp_dir/pastebins
 }
 
 function updateIRC {
@@ -86,21 +50,35 @@ function updateIRC {
 }
 
 function usage {
-	echo "Usage: $0 irc/pastebin"
+	echo "Usage:"
+	echo "============="
 	echo ""
 	echo "$0 irc"
 	echo -e "\t - Update local IRC archive"
-	echo "$0 pastebins"
-	echo -e "\t - Update local pastebin archive"
-	exit 0
+	echo ""
+	echo "$0 pasties pastebin|piratepad|opensuse"
+	echo -e "\t - Update local pasties archive"
+	echo ""
+	echo "$0 timestamp update-ircLogs"
+	echo -e "\t - Update timestamps of ircLogs as dated"
+	echo ""
+
+	exit 1
 }
 
-if [ "$1" == "pastebins" ]
+if [ "$1" == "pasties" ]
 then
-	getAllPastebins
+	if [ -z "$2" ]
+	then
+		usage
+	fi
+	fetchNewerLinks $2
 elif [ "$1" == "irc" ]
 then
 	updateIRC
+elif [ "$1" == "timestamp" -a "$2" == "update-ircLogs" ]
+then
+	updateIRCLogTimestamp	
 else
 	usage	
 fi
